cea95517a52f7c0adaa3e222de11127e5d2faa3d
Roland Rabien <roland@rabien.com>
2018-01-14 15:19:18 -0800

    Adding code

---
+++ FileSystemWatcher/FileSystemWatcher.cpp
@@ -0,0 +1,26 @@
+/*
+  ==============================================================================
+
+    FileSystemWatcher.cpp
+    Created: 13 Jan 2018 10:31:15pm
+    Author:  Roland Rabien
+
+  ==============================================================================
+*/
+
+#include "FileSystemWatcher.h"
+
+FileSystemWatcher::FileSystemWatcher()
+{
+    
+}
+
+void FileSystemWatcher::addFolder (const File& folder)
+{
+    
+}
+
+void FileSystemWatcher::removeFolder (const File& folder)
+{
+    
+}
---
+++ FileSystemWatcher/FileSystemWatcher.h
@@ -0,0 +1,22 @@
+/*
+  ==============================================================================
+
+    FileSystemWatcher.h
+    Created: 13 Jan 2018 10:31:15pm
+    Author:  Roland Rabien
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+class FileSystemWatch : public ChangeBroadcaster
+{
+public:
+    FileSystemWatcher();
+    
+    void addFolder (const File& folder);
+    void removeFolder (const File& folder);
+};
